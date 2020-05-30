81cb211dfe7c864d6e73ac09d40433541adbda24
Windmill-City <1449182174@qq.com>
2020-05-30 16:03:03 +0800

    Fix classpath

--- src/main/java/wormz/fatweaks/ASM/FATCore.java
+++ src/main/java/wormz/fatweaks/ASM/FATCore.java
@@ -1,6 +1,8 @@
 package wormz.fatweaks.ASM;
 
 import city.windmill.IntegratedServerTrans;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
@@ -9,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
+import java.io.File;
 import java.util.Map;
 
 @IFMLLoadingPlugin.TransformerExclusions({"wormz.fatweaks"})
@@ -18,10 +21,22 @@ public class FATCore implements IFMLLoadingPlugin {
     public static boolean isDeobf = false;
 
     public FATCore() {
+        try {
+            loadModJar(new File("./mods/".concat("Advanced Machines-61.0.1.jar")));
+            loadModJar(new File("./mods/".concat("CustomOreGen-1.12-1.8.1.jar")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         LOGGER.info("setting up mixin environment");
         MixinBootstrap.init();
         Mixins.addConfiguration("mixin.json");
     }
+
+    private void loadModJar(File jar) throws Exception {
+        ((LaunchClassLoader) this.getClass().getClassLoader()).addURL(jar.toURI().toURL());
+        CoreModManager.getReparseableCoremods().add(jar.getName());
+    }
+
     @Override
     public String[] getASMTransformerClass() {
         return new String[]{
